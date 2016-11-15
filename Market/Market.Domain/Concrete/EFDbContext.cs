fb2bd360a62e55795ab94e379065180507123433
SvitlanaKarapugina <sw.karapugina@gmail.com>
2016-11-15 18:48:00 +0200

    It's my first asp.net mvc project tea shop

---
+++ Market/Market.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Market.Domain.Entities;
+using System.Data.Entity;
+
+namespace Market.Domain.Concrete
+{
+  public  class EFDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
